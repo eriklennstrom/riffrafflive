464aec41cd020b89cf4f0acfd039d36d63458715
Erik Tedfelt Lennström <erik@tedfeltlennstrom.se>
2024-01-07 16:56:26 +0100

    add gh-pages and config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    baseURL: '/riffrafflive/',
+    buildAssetsDir: 'assets'
+  },
   modules: ['@nuxtjs/tailwindcss']
 })
